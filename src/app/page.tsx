80d93280e34ae77b4c7dc7605c23486c98c3f5c0
My name is Luis Fernando Lanza Amador <fernanamador26@gmail.com>
2025-05-12 21:09:29 -0600

    correcion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { useContext } from 'react';
-import { TemaContext } from './Contextos/TemaContext';
 import { useRouter } from 'next/navigation';
-import TemaItem from './components/TemaItem';
+import { TemaContext } from './Contextos/TemaContext';
+import TemaItem from './Components/TemaItem';
 
 export default function Home() {
   const { temas } = useContext(TemaContext);
@@ -11,14 +11,20 @@ export default function Home() {
   return (
     <div className="container mt-4">
       <h4 className="text-center mb-4">Pantalla Principal</h4>
+      
       {temas.map((tema) => (
         <TemaItem key={tema.id} {...tema} />
       ))}
+
       <div className="text-center mt-3">
-        <button className="btn btn-outline-primary" onClick={() => router.push('/(general)/configuracion')}>
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => router.push('/(general)/configuracion')}
+        >
           Visualizar temas interesantes
         </button>
       </div>
     </div>
   );
 }
+
